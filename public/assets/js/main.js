d7b9694d809b27f91560f7f506fcb413962eaeba
Vova <vboretskyi@gmail.com>
2016-05-28 19:35:00 +0300

    update sliding underline

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -786,35 +786,62 @@ angular.module("MainApp")
 });
 
 angular.module("MainApp")
-.directive('tabs', function() {
+.directive('tabs', ["$rootScope", function($rootScope) {
     return {
         restrict:'E',
         replace: true,
+        scope: {
+            notifications: '=notifications'
+        },
         link: function(scope, element, att) {
 
             scope.underline = element[0].querySelector('.tabs__underline');
             scope.links = element[0].querySelectorAll('a');
-            scope.activeLink = scope.links[0];
+            // scope.activeLink;
+
+            function setActiveLink() {
+                for (var i = 0; i < scope.links.length; i++) {
+                    if (scope.links[i].className === 'active') {
+                        scope.activeLink = scope.links[i];
+                    }
+                }
+            }
 
             function renderUnderline() {
-                var currentLeft = angular.element(scope.activeLink).prop('offsetLeft');
+                var currentLeft = !scope.notifications ?
+                angular.element(scope.activeLink).prop('offsetLeft') - 8 :
+                angular.element(scope.activeLink).prop('offsetLeft') - 15;
                 var currentWidth = scope.activeLink.clientWidth;
-
                 angular.element(scope.underline).css({
-                    'left': currentLeft - 8 + 'px',
-                    'width': currentWidth + 'px'
+                    'left': currentLeft + 'px',
+                    'width': currentWidth + 'px',
+                    'transition': '0.3s'
                 });
             }
 
-            renderUnderline();
-
-            for (var i = 0; i < scope.links.length; i++) {
-                (function(index) {
-                    scope.links[i].onclick = function() {
-                        scope.activeLink = scope.links[index];
-                        renderUnderline();
-                    };
-                })(i);
+            if (!scope.notifications) {
+                setTimeout(function() {
+                    setActiveLink();
+                    renderUnderline();
+                    $rootScope.$on('$stateChangeSuccess', function() {
+                        // console.log('state changed');
+                        setTimeout(function(){
+                            setActiveLink();
+                            renderUnderline();
+                        }, 10);
+                    });
+                }, 75);
+            } else {
+                scope.activeLink = scope.links[0];
+                renderUnderline();
+                for (var i = 0; i < scope.links.length; i++) {
+                    (function(index) {
+                        scope.links[i].onclick = function() {
+                            scope.activeLink = scope.links[index];
+                            renderUnderline();
+                        };
+                    })(i);
+                }
             }
 
             scope.$on('resize::resize', function() {
@@ -823,7 +850,7 @@ angular.module("MainApp")
 
         }
     };
-});
+}]);
 
 app.directive('resize', ["$window", function($window) {
   return {
