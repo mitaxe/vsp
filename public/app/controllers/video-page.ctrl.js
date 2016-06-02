09fa13e1ad9dd4e627aec6c2a075c09de4da9129
andrey.kunygin <andrey.kunygin@gmail.com>
2016-06-02 19:11:37 +0300

    video page

--- public/app/controllers/video-page.ctrl.js
+++ public/app/controllers/video-page.ctrl.js
@@ -1,9 +1,25 @@
 angular.module("MainApp")
-    .controller('VideoPageCtrl', ['$scope', function($scope) {
+    .controller('VideoPageCtrl', ['$scope', '$window', function($scope, $window) {
+        var windWidth = window.innerWidth;
 
-        $scope.activeTab = 1;
+        $scope.activeTab = windWidth > 1280 ? 1 : 3;
+        $scope.desktop =  windWidth > 1280 ? true : false;
 
         $scope.setActiveTab = function(index) {
             $scope.activeTab = index;
-        }
+        };
+
+        $scope.$on('resize::resize', function() {
+            if(window.innerWidth < 1280) {
+                $scope.desktop = false;
+                $scope.activeTab = 3; //set active comments tab
+            } else {
+                $scope.desktop = true;
+                $scope.activeTab = 1;
+            }
+        });
+
+        angular.element($window).bind('resize', function() {
+            $scope.$apply();
+        });
     }]);
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -672,13 +672,29 @@ angular.module("MainApp")
 }]);
 
 angular.module("MainApp")
-    .controller('VideoPageCtrl', ['$scope', function($scope) {
+    .controller('VideoPageCtrl', ['$scope', '$window', function($scope, $window) {
+        var windWidth = window.innerWidth;
 
-        $scope.activeTab = 1;
+        $scope.activeTab = windWidth > 1280 ? 1 : 3;
+        $scope.desktop =  windWidth > 1280 ? true : false;
 
         $scope.setActiveTab = function(index) {
             $scope.activeTab = index;
-        }
+        };
+
+        $scope.$on('resize::resize', function() {
+            if(window.innerWidth < 1280) {
+                $scope.desktop = false;
+                $scope.activeTab = 3; //set active comments tab
+            } else {
+                $scope.desktop = true;
+                $scope.activeTab = 1;
+            }
+        });
+
+        angular.element($window).bind('resize', function() {
+            $scope.$apply();
+        });
     }]);
 angular.module("MainApp")
 .controller('xChannelCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
