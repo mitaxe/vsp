5728dc45e31cc4ba90ec0ead2a3aa8f12ccbcd00
andrey.kunygin <andrey.kunygin@gmail.com>
2016-06-07 19:19:18 +0300

    settings page

--- public/app/controllers/settings.ctrl.js
+++ public/app/controllers/settings.ctrl.js
@@ -1,15 +1,16 @@
 angular.module("MainApp")
-    .controller('SettingsCtrl', ['$scope', function ($scope) {
+    .controller('SettingsCtrl', ['$scope', '$timeout', '$sce', function ($scope, $timeout, $sce) {
 
 
+        $scope.showMainIcon = false;
         $scope.items = [
             {
                 name : 'Оригинальные пранки',
-                items: [1,2,3,4]
+                items: ['Пранки','test2','Пранки 2','test4']
             },
             {
                 name : 'Лучшие видео',
-                items: [1,2]
+                items: [1,'Пранк']
             },
             {
                 name : 'Пантера Шоу',
@@ -36,5 +37,30 @@ angular.module("MainApp")
 
         $scope.hideInnerICons = function($event) {
             angular.element($event.currentTarget).children().removeClass('visible')
-        }
+        };
+
+
+        $scope.editableIndex = -1;
+        $scope.editMode = function(index) {
+            $scope.editableIndex = index;
+
+            // $timeout(function() {
+            //     document.getElementById('test').focus();
+            // }, 500);
+        };
+        $scope.disableAndUpdate = function(outer, inner, $event) {
+
+            if($scope.editableIndex != -1) {// edit now
+                // $scope.items[outer].items[inner] = angular.element($event.currentTarget).val();
+                $scope.items[outer].items[inner] = angular.element($event.currentTarget).text();
+            }
+            $scope.editableIndex = -1;
+            // console.log(angular.element($event.currentTarget).html());
+        };
+        $scope.highlight = function(text, search) {
+            if (!search) {
+                return $sce.trustAsHtml(text.toString());
+            }
+            return $sce.trustAsHtml(text.replace(new RegExp(search, 'gi'), '<span class="highlightedText">$&</span>'));
+        };
     }]);
