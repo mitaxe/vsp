ce19984c6cd59409f3ddc76f88f3031251ff4510
exz0N <andrey.kunygin@gmail.com>
2016-06-15 17:12:10 +0300

    upd ratings channel

--- public/app/controllers/ratings.ctrl.js
+++ public/app/controllers/ratings.ctrl.js
@@ -6,12 +6,12 @@ angular.module("MainApp")
     
     // get first portion of ratings videos from route resolve
     $scope.ratingsVideos = [];
-    console.log('after init empty array');
-    console.log($scope.ratingsVideos);
+    // console.log('after init empty array');
+    // console.log($scope.ratingsVideos);
 
     $scope.ratingsVideos = ratingsVideos.data.data;
-    console.log('set data to array');
-    console.log($scope.ratingsVideos);
+    // console.log('set data to array');
+    // console.log($scope.ratingsVideos);
 
     // get offset number
     $scope.initialOffset = ratingsVideos.data.meta.count;
@@ -52,20 +52,26 @@ angular.module("MainApp")
 
     /* Channels */
 
+
     $scope.ratingsChannels = ratingsChannels.data.data;
+
+    // get offset number
+    var channelOfset = 0;
+    $scope.noChannels = false;
+    $scope.channelsOffset = ratingsChannels.data.meta.count;
 
     console.log('channels - ' + $scope.ratingsChannels);
 
     $scope.loadMoreChannels = function() {
 
-        if (!$scope.noVideo) {
+        if (!$scope.noChannels) {
             $scope.loading = true;
-            offset += $scope.initialOffset;
+            channelOfset += $scope.channelsOffset;
 
-            console.log('offset request - ' + offset); //---
+            console.log('offset request - ' + channelOfset); //---
             console.time('ratingsRequestTime');
 
-            factory.getRatingsChannels(offset).success(function(response){
+            factory.getRatingsChannels(channelOfset).success(function(response){
 
                 if(response.data !== null) {
                     console.timeEnd('ratingsRequestTime');
@@ -74,7 +80,7 @@ angular.module("MainApp")
                     $scope.loading = false;
                 } else {
                     $scope.loading = false;
-                    $scope.noVideo = true;
+                    $scope.noChannels = true; // no channel in this case
                 }
 
             });
