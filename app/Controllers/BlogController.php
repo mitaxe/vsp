4e6b1080d7a3fb2e3077eed295eeb797459dd657
pylypenko <pylypenko@edsson.com>
2016-07-01 19:53:57 +0300

    Blog controller

---
+++ app/Controllers/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Blog REST API controller
+ *
+ * PHP version >= 5.3.0
+ *
+ * @author Taras Pylypenko <pylypenko@edsson.com>
+ */
+
+use Phalcon\Mvc\Controller;
+
+class BlogController extends RESTController
+{
+
+
+
+}
