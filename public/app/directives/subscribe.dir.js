f199322ab7b8393e690b1fc0ecf16d3ecc045203
Vova <vboretskyi@gmail.com>
2016-07-15 17:28:04 +0300

    Update styles

--- public/app/directives/subscribe.dir.js
+++ public/app/directives/subscribe.dir.js
@@ -1,51 +1,50 @@
 angular.module("MainApp")
-.directive('subscribe', function($timeout, $window) {
-  return {
-    restrict: 'E',
-    replace: true,
-    scope: {
-        subscriber: '=subscribeTo'
-    },
-    template: '<div class="subscribe_box">' +
+.directive('subscribe', function($window) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            subscriber: '=subscribeTo'
+        },
+        template: '<div class="subscribe_box">' +
                     '<span ng-click="subscribe()" class="green_btn" ng-class="{subscribed:subscribed}" ng-mouseover="hoverIn()" ng-mouseleave="hoverOut()">' +
-                        ' {{ getSubscriptionText() }} ' +
+                    ' {{ getButtonText() }} ' +
                     '</span>' +
                     '<span class="counter subscribe">{{ subscriber | number }}</span>' +
                 '</div>',
-    link: function(scope, element, attrs) {
+        link: function(scope, element, attrs) {
 
-        // subscribe
-        scope.subscribe = function() {
-            scope.subscribed = !scope.subscribed;
-            if (scope.subscribed) {
-                scope.subscriber += 1;
-            } else {
-                scope.subscriber -= 1;
-            }
-        };
-
-        scope.hoverIn = function(){
-            scope.subsButtonHovered = true;
-        };
-
-        scope.hoverOut = function(){
-            scope.subsButtonHovered = false;
-        };
-
-        scope.getSubscriptionText = function() {
-            var text = 'Подписаться';
-            if (scope.subscribed) {
-                text = 'Подписка оформлена';
-                if (scope.subsButtonHovered) {
-                    text = 'Отменить подписку';
+            scope.subscribe = function() {
+                scope.subscribed = !scope.subscribed;
+                if (scope.subscribed) {
+                    scope.subscriber += 1;
+                } else {
+                    scope.subscriber -= 1;
                 }
-                if ($window.innerWidth < 768) {
-                    text = 'Отписаться';
-                }
-            }
-            return text;
-        };
+            };
 
-    }
-  };
+            scope.hoverIn = function() {
+                scope.subsButtonHovered = true;
+            };
+
+            scope.hoverOut = function() {
+                scope.subsButtonHovered = false;
+            };
+
+            scope.getButtonText = function() {
+                var text = 'Подписаться';
+                if (scope.subscribed) {
+                    text = 'Подписка оформлена';
+                    if (scope.subsButtonHovered) {
+                        text = 'Отменить подписку';
+                    }
+                    if ($window.innerWidth < 768) {
+                        text = 'Отписаться';
+                    }
+                }
+                return text;
+            };
+
+        }
+    };
 });
